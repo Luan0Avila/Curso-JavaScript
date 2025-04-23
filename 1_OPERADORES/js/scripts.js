e18db406c8690ff3a60ba5c97cfe67b0354af2a0
Luan0Avila <avila.almeida.luan@gmail.com>
2025-04-23 16:34:20 -0300

    começando o aprendizado de JavaScript para futuras utilizações dele em meus sites

---
+++ 1_OPERADORES/js/scripts.js
@@ -0,0 +1,26 @@
+// 1 - Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+
+// 2 - Ops. aritmeticas
+
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(2 * 10);
+console.log(100 / 5);
+
+console.log(5*(10 + 2));
+
+// 3 - Special Numbers
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+
+console.log(12 * "asd");
+console.log(typeof Nan);
+
+// 4 - Strings
+console.log("Hello Wolrd!");
+console.log('Hello Wolrd!');
+console.log("11")
