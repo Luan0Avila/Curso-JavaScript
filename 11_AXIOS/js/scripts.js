d37da2d23bdb0dfe3dc9d4223d449744b310cc15
Luan0Avila <avila.almeida.luan@gmail.com>
2025-05-09 11:30:00 -0300

    aprendendo headers

--- 11_AXIOS/js/scripts.js
+++ 11_AXIOS/js/scripts.js
@@ -6,7 +6,15 @@ const getData = async() => {
 
     try {
 
-        const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users", 
+            // 4 - Definindo headers
+        {
+            headers: {
+                "Content-Type": "application/json",
+                custom: "header",
+            },
+
+        });
 
         console.log(response);
         return response.data;
@@ -47,3 +55,4 @@ const printData = async() => {
 };
 
 printData();
+
